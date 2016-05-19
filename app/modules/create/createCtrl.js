42bf2a4e6298284d501c60f6cd0e2738d311edbb
Tyler Goelz <tyler@tylergoelz.com>
2016-05-19 21:01:55 +0000

    Update example link to be one Tamiko will be using (her charity). Add image gallery to customization page with static images and checkbox to enable 'custom image upload' functionality. Update layout of final page and add some nice-to-have things like tooltips.

--- app/modules/create/createCtrl.js
+++ app/modules/create/createCtrl.js
@@ -38,7 +38,7 @@
 			};
 			
 			$scope.product_url = '';
-			$scope.productUrlHint = 'https://giveto.seattlechildrens.org/changealife';
+			$scope.productUrlHint = 'http://www.bloodworksnw.org/home/index.htm';
 			$scope.showProductUrlHint = true;
 			$scope.urlPattern = /^(http|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:\/~\+#]*[\w\-\@?^=%&amp;\/~\+#])?/i;
 
@@ -49,7 +49,7 @@
 			// Setup the initial step data
 			vm.stepData = [
 				{ step: 1, completed: false, optional: false, data: {product_url: 'https://'}},
-				{ step: 2, completed: false, optional: false, data: {title: 'Make a Donation - Seattle Children\'s - Seattle Children\'s', price: '100.00'} },
+				{ step: 2, completed: false, optional: false, data: {title: 'Imagine Saving a Life: Donate Blood Today', price: '250.00'} },
 				{ step: 3, completed: false, optional: false, data: {} },
 			];
 		
